1f416090724e6df3e104c60f0686726ca16f0898
Sebastien Cambier <slink794@gmail.com>
2024-03-27 14:41:51 +0100

    Fixed norm in here_doc.c

--- src/here_doc.c
+++ src/here_doc.c
@@ -14,37 +14,32 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-
-//#include <stdio.h>
 
 #include "libft.h"
 #include "header.h"
 
+#define HEREDOC_FILE "/tmp/here_doc"
+#define HEREDOC_PROMPT "heredoc> "
+
 static void	here_doc_read(char *marker)
 {
 	char	*line;
-	int		len_line;
+	int		len;
 	int		fd;
 
-	fd = open("/tmp/here_doc", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	fd = open(HEREDOC_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fd < 0)
+		return (perror("minishell: open"));
+	line = readline(HEREDOC_PROMPT);
+	while (1)
 	{
-		return perror("minishell: open");
-	}
-	line = readline("heredoc> ");
-	while (ft_strncmp(line, marker, ft_strlen(line)) != 0 || ft_strlen(line) == 0)
-	{
-		if (ft_strncmp(line, marker, ft_strlen(line)) != 0 || ft_strlen(line) == 0)
-		{
-			write(fd, line, ft_strlen(line));
-			write(fd, "\n", 1);
-		}
-		len_line = ft_strlen(line);
-		line[len_line] = '\0';
+		len = ft_strlen(line);
+		if (!line || ft_strncmp(line, marker, len + 1) == 0)
+			break ;
+		write(fd, line, len);
+		write(fd, "\n", 1);
 		free(line);
-		line = readline("heredoc> ");
+		line = readline(HEREDOC_PROMPT);
 	}
 	free(line);
 	close(fd);
@@ -55,7 +50,7 @@ int	here_doc(char *marker)
 	int		fd;
 
 	here_doc_read(marker);
-	fd = open("/tmp/here_doc", O_RDONLY, 0644);
+	fd = open(HEREDOC_FILE, O_RDONLY, 0644);
 	if (fd == -1)
 		perror("minishell: open");
 	return (fd);
